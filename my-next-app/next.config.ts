4ff3aa007eaacabf092969df757ae5999ee2aa3e
spako59io <145841379+spako59io@users.noreply.github.com>
2024-11-19 03:37:57 +0000

    Fix dependencies and configuration

--- my-next-app/next.config.ts
+++ my-next-app/next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
